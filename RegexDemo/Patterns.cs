175a3734574e26432e6ac6124f051d292d5ae229
Sourav2707 <smartsourav33@gmail.com>
2022-09-20 15:02:10 +0530

    initial commit

---
+++ RegexDemo/Patterns.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    class Patterns
+    {
+        public static string RegexRules = "^[1-9][0-9]{2}[ ]?[0-9]{3}$";   // ^[1-9][0-9]{2}[ ][0-9]{3}$//"^[0-9]*[A-Za-z]{3,}[a-zA-Z0-9]*$";
+
+        //public static string Regex_Pincode = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+        //public static string regex = "^[A-Za-z0-9]+([.#_+][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-z]{2,3}([.][a-z]{2})?$";
+        //public static string regex = "^[A-Za-z0-9]+([._#%+][A-Za-z0-9]+)?[@][A-Za-z]+[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+}
---
+++ RegexDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿using RegexDemo;
+
+Patterns patterns = new Patterns();
+//Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
+if (patterns.validateString("625017"))
+{
+    Console.WriteLine("Valid");
+}
+else
+    Console.WriteLine("Invalid");
+Console.ReadKey();
